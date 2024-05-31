976b992d48f770de317c7092fc42ba936463e59a
lizagran <liza.grankina@gmail.com>
2024-06-01 00:13:51 +0300

    delete token

--- telegrambot/token_bot.py
+++ telegrambot/token_bot.py
@@ -1,6 +1,4 @@
 import telebot
 
-bot = telebot.TeleBot('6785016047:AAHWxLLs1KzFHo47vlRMkSrpbOeOpMt2LPw')
-mytoken = '6785016047:AAHWxLLs1KzFHo47vlRMkSrpbOeOpMt2LPw'
-# bot = telebot.TeleBot('6787814998:AAHKTJmf79rKj8vwcBGJWe96ESdzGFePico')
-# mytoken = '6787814998:AAHKTJmf79rKj8vwcBGJWe96ESdzGFePico'
+bot = telebot.TeleBot('token')
+mytoken = 'token'
